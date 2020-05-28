a4dd1343bd636df3f9649cd481ce619054219d78
Seaidear <seaidear323@gmail.com>
2020-05-28 20:18:57 +0200

    Added fake API functions

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
-const LOCALSTOARGE_KEY = 'cards_data';
+import { AsyncStorage } from 'react-native';
+import { generateUUID } from './helpers';
+
+const CARDS_DATA_ASYNCSTORAGE_KEY = 'cards_data';
 
 const initialData = {
   sxbjgrwdbhf58lxznh9q79: {
@@ -28,10 +31,76 @@ const initialData = {
   },
 };
 
-export const getDecks = () => {};
+export const getInitialData = () => initialData;
 
-export const getDeck = (id) => {};
+export const getDecks = async () => {
+  try {
+    const results = await AsyncStorage.getItem(CARDS_DATA_ASYNCSTORAGE_KEY);
+    if (results) {
+      const data = JSON.parse(results);
+      return data;
+    } else {
+      await AsyncStorage.setItem(
+        CARDS_DATA_ASYNCSTORAGE_KEY,
+        JSON.stringify(getInitialData()),
+      );
+      return getInitialData();
+    }
+  } catch (error) {
+    await AsyncStorage.setItem(
+      CARDS_DATA_ASYNCSTORAGE_KEY,
+      JSON.stringify(getInitialData()),
+    );
+    return getInitialData();
+  }
+};
 
-export const saveDeckTitle = (title) => {};
+// export const getDeck = async (id) => {};
 
-export const addCardToDeck = (title, card) => {};
+export const deleteDeck = async (deckId) => {
+  const results = await AsyncStorage.getItem(CARDS_DATA_ASYNCSTORAGE_KEY);
+  if (results) {
+    const data = JSON.parse(results);
+    delete data[deckId];
+
+    await AsyncStorage.setItem(
+      CARDS_DATA_ASYNCSTORAGE_KEY,
+      JSON.stringify(data),
+    );
+    return data;
+  }
+  return {};
+};
+
+export const saveDeckTitle = async (title) => {
+  const id = generateUUID();
+  const deck = {
+    id: id,
+    title: title,
+    questions: [],
+  };
+
+  await AsyncStorage.mergeItem(
+    CARDS_DATA_ASYNCSTORAGE_KEY,
+    JSON.stringify({
+      [id]: deck,
+    }),
+  );
+  return deck;
+};
+
+export const addCardToDeck = async (deckId, card) => {
+  const results = await AsyncStorage.getItem(CARDS_DATA_ASYNCSTORAGE_KEY);
+  if (results) {
+    const data = JSON.parse(results);
+    const deck = data[deckId];
+    deck.questions = deck.questions.concat([card]);
+    await AsyncStorage.mergeItem(
+      CARDS_DATA_ASYNCSTORAGE_KEY,
+      JSON.stringify({
+        [deckId]: deck,
+      }),
+    );
+    return card;
+  }
+};
