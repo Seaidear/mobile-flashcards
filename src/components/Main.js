5c5204cfdcc926f974febbf42f1c60e63da91e68
Seaidear <seaidear323@gmail.com>
2020-05-29 11:48:54 +0200

    Added "expo-constants" to projects deps & updated imports

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,38 +1,24 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
+import React, { useEffect } from 'react';
+import { View, StatusBar } from 'react-native';
+import { Container } from 'native-base';
+import Stack from './StackNav';
+import * as Constants from 'expo-constants';
+import { connect } from 'react-redux';
+import { handleGetAllDecks } from '../actions/decks';
 
 const Main = () => {
+  useEffect(() => {
+    handleGetAllDecks();
+  });
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
+    <Container>
+      <View style={{ height: Constants.statusBarHeight }}>
+        <StatusBar />
+      </View>
+      <Stack />
+    </Container>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-export default Main;
+export default connect(null)(Main);
