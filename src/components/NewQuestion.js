66741076c8c645a529dcbe0fb87e659260e4876c
Seaidear <seaidear323@gmail.com>
2020-05-29 17:17:13 +0200

    Updated styling of "add question"-component

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,36 +1,54 @@
 import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 
 class NewQuestion extends React.Component {
   state = {
     question: '',
     answer: '',
-  }
+  };
 
   handleChange = (input, value) => {
     this.setState({
-      [input]: value
+      [input]: value,
     });
-  }
+  };
 
   onSubmit = () => {
     const { handleSubmit, navigate, deckId } = this.props;
     handleSubmit(this.state);
-    navigate("Deck", { deckId });
-  }
+    navigate('Deck', { deckId });
+  };
 
   render() {
     const { question, answer } = this.state;
     return (
       <View style={styles.container}>
-        <TextInput placeholder="Question" value={question} onChangeText={(val) => this.handleChange("question", val)} style={styles.questions} />
-        <TextInput placeholder="Answer" value={answer} onChangeText={(val) => this.handleChange("answer", val)} style={styles.questions} />
-        <TouchableOpacity onPress={this.onSubmit} disabled={question.length === 0 || answer.length === 0}>
+        <TextInput
+          placeholder="Question"
+          value={question}
+          onChangeText={(val) => this.handleChange('question', val)}
+          style={styles.questions}
+        />
+        <TextInput
+          placeholder="Answer"
+          value={answer}
+          onChangeText={(val) => this.handleChange('answer', val)}
+          style={styles.questions}
+        />
+        <TouchableOpacity
+          onPress={this.onSubmit}
+          disabled={question.length === 0 || answer.length === 0}
+        >
           <Text style={styles.submitBtn}>Submit</Text>
         </TouchableOpacity>
       </View>
-    )
+    );
   }
 }
 
@@ -44,18 +62,20 @@ const styles = StyleSheet.create({
   },
   questions: {
     borderRadius: 4,
-    borderColor: 'black',
+    borderColor: '#694fad',
     borderWidth: 1,
     width: 280,
     padding: 10,
-    marginBottom: 20
+    marginBottom: 20,
   },
   submitBtn: {
     paddingVertical: 16,
     paddingHorizontal: 32,
-    backgroundColor: 'black',
-    color: 'white',
+    backgroundColor: '#694fad',
+    width: 280,
+    textAlign: 'center',
+    color: '#f5f5f5',
     borderRadius: 4,
     fontSize: 16,
-  }
+  },
 });
