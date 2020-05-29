fdb0e01bf19c2a57b26cb58b4d6cb21805816dc0
Seaidear <seaidear323@gmail.com>
2020-05-29 16:00:21 +0200

    Udpated helper functions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,67 +1,56 @@
-import { AsyncStorage } from 'react-native';
-import { Notifications } from 'expo';
-import * as Permissions from 'expo-permissions';
+import { Notifications } from "expo";
+import * as Permissions from "expo-permissions";
+import { AsyncStorage } from "react-native";
 
-const NOTIFICATIONS_ASYNCSTORAGE_KEY = 'flashcards_notification';
-
-export const generateUUID = () =>
-  Math.random().toString(36).substring(2, 15) +
-  Math.random().toString(36).substring(2, 15);
-
-export const colors = {
-  homeBackgroundColor: '#7e57c2',
-  homeCardBackgroundColor: '#fff9e6',
-  allScreensBackgroundColor: '#fff9e6',
-  headerColor: '#5e35b1',
-  darkButtonColor: '#673ab7',
-  darkTextColor: '#9575cd',
-  quizCardColor: '#b39ddb',
+export const StorageKeys = {
+  Decks: "Decks",
+  Notifications: "Notifications",
 };
 
-export const clearLocalNotification = () => {
-  return AsyncStorage.removeItem(NOTIFICATIONS_ASYNCSTORAGE_KEY).then(() =>
-    Notifications.cancelScheduledNotificationAsync(),
-  );
+export const createNotification = () => {
+  return {
+    title: "Take a quiz",
+    body: "Let's test our knowledge today!",
+    ios: {
+      sound: true,
+    },
+    android: {
+      sound: true,
+      priority: "high",
+      sticky: false,
+      vibrate: true,
+    },
+  };
 };
-
-const createNotification = () => ({
-  title: 'Time to learn!',
-  body: "It's time for your daily dose of quiz fun.",
-  ios: {
-    sound: true,
-  },
-  android: {
-    sound: true,
-    priority: 'high',
-    sticky: false,
-    vibrate: true,
-  },
-});
 
 export const setLocalNotification = () => {
-  AsyncStorage.getItem(NOTIFICATIONS_ASYNCSTORAGE_KEY)
+  AsyncStorage.getItem(StorageKeys.Notifications)
     .then(JSON.parse)
     .then((data) => {
       if (data === null) {
         Permissions.askAsync(Permissions.NOTIFICATIONS).then(({ status }) => {
-          if (status === 'granted') {
+          if (status === "granted") {
             Notifications.cancelAllScheduledNotificationsAsync();
 
             let tomorrow = new Date();
             tomorrow.setDate(tomorrow.getDate() + 1);
-            tomorrow.setHours(11);
+            tomorrow.setHours(20);
             tomorrow.setMinutes(0);
 
             Notifications.scheduleLocalNotificationAsync(createNotification(), {
               time: tomorrow,
-              repeat: 'day',
+              repeat: "day",
             });
-            AsyncStorage.setItem(
-              NOTIFICATIONS_ASYNCSTORAGE_KEY,
-              JSON.stringify(true),
-            );
+
+            AsyncStorage.setItem(StorageKeys.Notifications, JSON.stringify(true));
           }
         });
       }
     });
+};
+
+export const clearLocalNotification = () => {
+  return AsyncStorage.removeItem(StorageKeys.Notifications).then(
+    Notifications.cancelAllScheduledNotificationsAsync
+  );
 };
